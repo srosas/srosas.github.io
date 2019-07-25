74faf6179a467c3d78f00ae3f3c9d5358aef5165
Steven Rosas <srosas@Stevens-MacBook-Pro-2.local>
2019-07-25 13:07:51 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Star Wars React App
+A simple Star Wars character search App that returns movies a character has been in.
+
+To install
+### `npm install`
+### `npm start`
---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Header from './components/Header'
+import CharactersList from './components/CharactersList'
+import MovieList from './components/MovieList'
+import './styles/App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characterName: '',
+      characterURL: '',
+      moviesArray: [],
+      moviesData: [],
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault()
+    this.setState(
+      {
+        characterName: event.target.getAttribute('name'),
+        characterURL: event.target.getAttribute('url'),
+        moviesData: [],
+        moviesArray: []
+      },
+      this.getMovieArray
+    );
+  };
+
+  getMovieArray() {
+    const url = this.state.characterURL;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState(
+          {
+            moviesArray: data.films,
+          },
+          this.getAllMovies
+        )
+      });
+  }
+
+  getAllMovies() {
+    this.state.moviesArray.forEach(async (movieURL) => {
+      await this.getMovieData(movieURL)
+    })
+  }
+
+  getMovieData(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState(prevState => ({
+          moviesData: [
+            ...prevState.moviesData,
+            {
+              title: data.title,
+              releaseDate: (new Date(data.release_date)).toDateString()
+            }],
+        }))
+      });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <CharactersList handleClick={this.handleClick} />
+        <MovieList moviesData={this.state.moviesData} name={this.state.characterName} />
+      </div>
+    );
+  }
+}
+
+export default App;
